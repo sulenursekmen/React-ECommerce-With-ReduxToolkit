64b20d76bd4b8703eaaf5a430072192eca2012e9
sulenursekmen <sulenursekmenn@gmail.com>
2024-01-06 19:49:33 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 class="text-3xl font-bold underline w-10/12 m-auto">Hello world!</h1>
     </div>
   );
 }
